df2ea17cd382567f0638a05be3dc8e4836396538
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-03 09:38:54 -0400

    Update Code in Textbook.r

--- Chapter 9/Code in Textbook.r
+++ Chapter 9/Code in Textbook.r
@@ -48,46 +48,11 @@ plot(m5.tempdub,n.ahead=24,n1=c(1972,1),newxreg=newhar., col=’red’, type=’
 data(color)
 m1.color=arima(color,order=c(1,0,0))
 plot(m1.color,n.ahead=12,col='red',type='b',xlab='Year', ylab='Temperature')
+
+# The final command adds the horizontal line at the estimated mean (intercept)
 abline(h=coef(m1.color)[names(coef(m1.color))=='intercept'])
-# The final command adds the horizontal line at the estimated mean (intercept). 
-# The expression coef(m1.color) extracts the coefficient vector. The components of the coefficient vector are named. 
-# The names of a vector can be extracted by the names function, 
-# so names(coef(m1.color)) returns the vector of names of the components of the coefficient vector. 
-# The == operator compares the two vectors on its two sides element by element, 
-# resulting in a vector consisting of TRUEs and FALSEs depending on whether the elements are equal or not. 
+# The expression coef(m1.color) extracts the coefficient vector.
+# names(coef(m1.color)) returns the vector of names of the components of the coefficient vector. 
 # (If the vectors under comparison are of unequal length, R recycles the shorter one repeatedly to match the longer one.) 
-# Hence, the command [names(coef(m1.color))== 'intercept']
-# returns a vector with the TRUE value in the position in which the “intercept” component lies and with all other elements FALSE. 
-# Finally, the intercept coefficient estimate is extracted by the “bracket” operation:
-coef(m1.color)[names(coef(m1.color))=='intercept']
-# The operation within brackets subsets a vector using one of two mechanisms. Let v be a vector. 
-# A subvector of it can be formed by the command v[s], where s is a Boolean vector, 
-# (that is, consisting of TRUEs and FALSEs) that is of the same length as v.
-# The vector v[s] is then a sub-vector of v consisting of those elements of v for which the corresponding element in s is TRUE; 
-# elements in v whose corresponding element in s is FALSE are discarded from v[s].
-# A second way to subset a vector is to construct s so that it contains the position of the elements to be retained and 
-# v[s] will return the desired subvector. A variation of this approach is to form a subvector by deletion. 
-# Unwanted elements are designated by giving their positions multiplied by -1. An illustration follows.
-> v=1:5
-# This creates a vector containing the first five positive integers.
->v
-[1] 1 2 3 4 5
-> names(v)
-  NULL
-# By default, the components of v are unnamed, so names(v) returns an empty vector denoted by the object NULL.
-> names(v)=c('A','B','C','D','E')
-# This is the method of assigning names to the components of a vector.
->v 
-ABCDE 
-12345
-> names(v)=='C'
-[1] FALSE FALSE TRUE FALSE FALSE 
-> v[names(v)=='C']
- C
-3
-> v[3]
-C 3
-> v[-3] 
-ABDE 
-1245
-# subsets v by supplying the positions of the unwanted elements.
+# [names(coef(m1.color))== 'intercept']returns a vector with the TRUE value in the position in which the “intercept” component
+# lies and with all other elements FALSE. 
